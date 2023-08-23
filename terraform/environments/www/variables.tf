9a82b3d3a8660593ba66dbd60cf9a2aae8169ff3
snake <admin@viasnake.com>
2023-08-24 05:40:13 +0900

    feat: 認証情報に環境変数を使う

--- terraform/environments/www/variables.tf
+++ terraform/environments/www/variables.tf
@@ -2,14 +2,3 @@ variable "proxmox_api_url" {
   type = string
   default = "http://10.0.10.1:8006/api2/json"
 }
-
-variable "proxmox_user" {
-  type = string
-  default = "root@pam"
-}
-
-variable "proxmox_password" {
-  type = string
-  sensitive = true
-  default = "password"
-}
