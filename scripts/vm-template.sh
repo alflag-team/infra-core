aab5d2fb99b9730d88f667d42752522444bf28c5
snake <admin@viasnake.com>
2023-08-24 06:26:27 +0900

    feat: vm-template.shを更新

--- scripts/vm-template.sh
+++ scripts/vm-template.sh
@@ -3,26 +3,23 @@
 VM_ID=9000
 VM_NAME=ubuntu-server-22.04
 VM_CORES=2
-VM_MEMORY=$(( 1024 * 2 ))
+VM_MEMORY=$(( 1024 ))
 VM_STORAGE=local-lvm
 
 # Download Ubuntu Server 22.04 LTS
 wget https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-amd64.img
 
 # Create a new VM with VirtIO SCSI controller
-qm create $VM_ID --name $VM_NAME --memory $VM_MEMORY --cores $VM_CORES --net0 virtio,bridge=vmbr0 --scsihw virtio-scsi-pci
+qm create $VM_ID --name $VM_NAME --memory $VM_MEMORY --net0 virtio,bridge=vmbr0 --scsihw virtio-scsi-pci
 
 # Import the downloaded image to local-lvm storage
-qm importdisk $VM_ID jammy-server-cloudimg-amd64.img hdd-01
-
-# Attach the disk to the VM
-qm set $VM_ID --scsihw virtio-scsi-pci --scsi0 $VM_STORAGE:vm-$VM_ID-disk-0
+qm set $VM_ID --scsi0 $VM_STORAGE:0,import-from=$(pwd)/jammy-server-cloudimg-amd64.img
 
 # Set Cloud-Init drive to the VM
 qm set $VM_ID --ide2 $VM_STORAGE:cloudinit
 
-# Set bootdisk to the disk we just attached
-qm set $VM_ID --boot c --bootdisk scsi0
+# Set bootdisk to scsi0
+qm set $VM_ID --boot order=scsi0
 
 # Set serial0 to socket and enable serial console
 qm set $VM_ID --serial0 socket --vga serial0
