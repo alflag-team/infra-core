2881d2a41579596621913d86d93c6143ece0431b
snake <admin@viasnake.com>
2023-11-10 05:06:22 +0900

    feat: web系統の設定に変更する

--- terraform/environments/www/main.tf
+++ terraform/environments/www/main.tf
@@ -1,16 +1,16 @@
 module "qemu" {
   source = "../../modules/qemu"
 
-  name         = "test"
+  name         = "web1"
   target_node  = "kitsune"
   vmid         = 0
   clone        = "ubuntu-server-22.04"
   cores        = 2
-  sockets      = 2
-  memory       = 4096
+  sockets      = 1
+  memory       = 2048
   os_type      = "cloud-init"
   disk_type    = "scsi"
   disk_storage = "hdd-01"
   disk_size    = "16G"
-  ipconfig0    = "ip=10.0.10.100/8,gw=10.0.0.1"
+  ipconfig0    = "ip=10.210.100.1/24,gw=10.0.0.1"
 }
