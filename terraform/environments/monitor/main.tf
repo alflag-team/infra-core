a39aaef65a10ae53149d4367164e29c2805f25a6
snake <admin@viasnake.com>
2023-11-24 01:40:47 +0900

    feat: qemu から lxc にする

--- terraform/environments/monitor/main.tf
+++ terraform/environments/monitor/main.tf
@@ -1,16 +1,8 @@
-module "qemu" {
-  source = "../../modules/qemu"
+module "lxc" {
+  source = "../../modules/lxc"
 
-  name         = "monitor1001"
-  target_node  = "kitsune"
-  vmid         = 0
-  clone        = "ubuntu-server-22.04"
-  cores        = 2
-  sockets      = 1
-  memory       = 2048
-  os_type      = "cloud-init"
-  disk_type    = "scsi"
-  disk_storage = "local-lvm"
-  disk_size    = "32G"
-  ipconfig0    = "ip=10.210.2.2/24,gw=10.210.0.1"
+  hostname    = "monitor1000"
+  target_node = "kitsune"
+  network_ip  = "10.210.2.2/24"
+  password    = "password"
 }
