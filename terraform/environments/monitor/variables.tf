3937d5ceb711f411ddc596ab75e0a8fc181ab5a4
snake <admin@viasnake.com>
2023-11-28 02:29:58 +0900

    feat: 認証情報を追加

--- terraform/environments/monitor/variables.tf
+++ terraform/environments/monitor/variables.tf
@@ -1,1 +1,17 @@
-variable "root_password" {}
+variable "root_password" {
+  type      = string
+  sensitive = true
+}
+variable "pm_user" {
+  type      = string
+  sensitive = true
+}
+variable "pm_password" {
+  type      = string
+  sensitive = true
+}
+
+variable "proxmox_api_url" {
+  type    = string
+  default = "http://10.210.1.1:8006/api2/json"
+}
