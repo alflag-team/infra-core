8a0ed58593a96df07a69741fbc44ec64c3583b92
snake <admin@viasnake.com>
2023-11-29 07:36:33 +0900

    fix: database1001 の IP を変更

--- terraform/environments/database/main.tf
+++ terraform/environments/database/main.tf
@@ -4,6 +4,6 @@ module "lxc" {
   hostname      = "database1001"
   target_node   = "kitsune"
   memory        = 2048
-  network_ip    = "10.210.4.2/24"
+  network_ip    = "10.210.4.3/24"
   root_password = var.root_password
 }
