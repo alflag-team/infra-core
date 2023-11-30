07289384d88cd8239d93109e9e61fba5a5181f73
snake <admin@viasnake.com>
2023-12-01 01:51:59 +0900

    feat: ansible 用ユーザの作成

--- scripts/hookscript.sh
+++ scripts/hookscript.sh
@@ -8,13 +8,21 @@ fi
 CONTAINER_ID="$1"
 PHASE="$2"
 
-# User details
-USER_NAME="infra"
-USER_UID=1000
-GROUP_NAME="infra"
-GROUP_GID=1000
-USER_SHELL="/bin/bash"
-PASSWORD="password"
+# Infra user details
+USER_INFRA_NAME="infra"
+USER_INFRA_UID=1000
+USER_INFRA_SHELL="/bin/bash"
+USER_INFRA_PASSWORD="password"
+GROUP_INFRA_NAME="infra"
+GROUP_INFRA_GID=1000
+
+# Ansible user details
+USER_ANSIBLE_NAME="ansible"
+USER_ANSIBLE_UID=1500
+USER_ANSIBLE_SHELL="/bin/bash"
+USER_ANSIBLE_PASSWORD="password"
+GROUP_ANSIBLE_NAME="ansible"
+GROUP_ANSIBLE_GID=1500
 
 # SSH configuration
 SSH_PATH="/etc/ssh/sshd_config"
@@ -50,22 +58,40 @@ pre_start_actions() {
 post_start_actions() {
   log "Executing post-start actions..."
 
-  # Check if the group already exists
-  if ! group_exists "$GROUP_NAME"; then
-    lxc-attach -n "$CONTAINER_ID" -- groupadd -g "$GROUP_GID" "$GROUP_NAME"
+  # # Create User infra
+  # ## Check if the group already exists
+  # if ! group_exists "$GROUP_INFRA_NAME"; then
+  #   lxc-attach -n "$CONTAINER_ID" -- groupadd -g "$GROUP_INFRA_GID" "$GROUP_INFRA_NAME"
+  # else
+  #   log "Group $GROUP_INFRA_NAME already exists. Skipping group creation."
+  # fi
+
+  # ## Check if the user already exists
+  # if ! user_exists "$USER_INFRA_NAME"; then
+  #   lxc-attach -n "$CONTAINER_ID" -- useradd -m -u "$USER_INFRA_UID" -g "$GROUP_INFRA_NAME" -G sudo -s "$USER_INFRA_SHELL" "$USER_INFRA_NAME"
+  #   echo "${USER_INFRA_NAME}:${USER_INFRA_PASSWORD}" | lxc-attach -n "$CONTAINER_ID" -- chpasswd
+  # else
+  #   log "User $USER_INFRA_NAME already exists. Skipping user creation."
+  # fi
+
+  # Create User ansible
+  ## Check if the group already exists
+  if ! group_exists "$GROUP_ANSIBLE_NAME"; then
+    lxc-attach -n "$CONTAINER_ID" -- groupadd -g "$GROUP_ANSIBLE_GID" "$GROUP_ANSIBLE_NAME"
   else
-    log "Group $GROUP_NAME already exists. Skipping group creation."
+    log "Group $GROUP_ANSIBLE_NAME already exists. Skipping group creation."
   fi
 
-  # Check if the user already exists
-  if ! user_exists "$USER_NAME"; then
-    lxc-attach -n "$CONTAINER_ID" -- useradd -m -u "$USER_UID" -g "$GROUP_NAME" -G sudo -s "$USER_SHELL" "$USER_NAME"
-    echo "${USER_NAME}:${PASSWORD}" | lxc-attach -n "$CONTAINER_ID" -- chpasswd
+  ## Check if the user already exists
+  if ! user_exists "$USER_ANSIBLE_NAME"; then
+    lxc-attach -n "$CONTAINER_ID" -- useradd -m -u "$USER_ANSIBLE_UID" -g "$GROUP_ANSIBLE_NAME" -G sudo -s "$USER_ANSIBLE_SHELL" "$USER_ANSIBLE_NAME"
+    echo "${USER_ANSIBLE_NAME}:${USER_ANSIBLE_PASSWORD}" | lxc-attach -n "$CONTAINER_ID" -- chpasswd
   else
-    log "User $USER_NAME already exists. Skipping user creation."
+    log "User $USER_ANSIBLE_NAME already exists. Skipping user creation."
   fi
 
-  # Check if the SSH configuration already exists
+  # Configure SSH
+  ## Check if the SSH configuration already exists
   if ! config_exists "$SSH_CONFIG" "$SSH_PATH"; then
     lxc-attach -n "$CONTAINER_ID" -- bash -c "echo -e '$SSH_CONFIG' >> $SSH_PATH"
     log "Appended configuration to $SSH_PATH."
