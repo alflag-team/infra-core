b92f4f36d3da44e07dd9592a4527ef368c2b55a9
snake <admin@viasnake.com>
2023-12-01 02:22:52 +0900

    feat: hostname を変更

--- terraform/environments/monitor/main.tf
+++ terraform/environments/monitor/main.tf
@@ -1,7 +1,7 @@
 module "lxc" {
   source = "../../modules/lxc"
 
-  hostname      = "monitor1000"
+  hostname      = "monitor1002"
   target_node   = "kitsune"
   memory        = 1024
   network_ip    = "10.210.2.2/24"
