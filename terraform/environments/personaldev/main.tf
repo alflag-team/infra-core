a39d1012e8a6c81809bb4bd9b9f748dcf5db8ce7
snake <admin@viasnake.com>
2024-01-18 22:27:54 +0900

    fix: IP を修正

--- terraform/environments/personaldev/main.tf
+++ terraform/environments/personaldev/main.tf
@@ -4,6 +4,6 @@ module "qemu" {
   hostname      = "personaldev1001"
   target_node   = "kitsune"
   memory        = 2048
-  network_ip    = "10.210.4.1/24"
+  network_ip    = "10.210.11.1/24"
   root_password = var.root_password
 }
