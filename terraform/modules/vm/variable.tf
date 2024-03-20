73cab730431f087ae9270072486475560b6efcf9
snake <admin@viasnake.com>
2024-03-21 03:53:00 +0900

    feat: vm module を追加

---
+++ terraform/modules/vm/variable.tf
@@ -0,0 +1,90 @@
+variable "name" {
+  type = string
+}
+
+variable "description" {
+  type = string
+}
+
+variable "tags" {
+  type    = list(string)
+  default = []
+}
+
+variable "node_name" {
+  type = string
+}
+
+variable "vm_id" {
+  type = number
+}
+
+variable "cpu_architecture" {
+  type    = string
+  default = "x86_64"
+}
+
+variable "cpu_cores" {
+  type    = number
+  default = 1
+}
+
+variable "cpu_sockets" {
+  type    = number
+  default = 1
+}
+
+variable "disk_datastore_id" {
+  type    = number
+  default = "hdd-01"
+}
+
+variable "disk_interface" {
+  type    = string
+  default = "virtio"
+}
+
+variable "disk_size" {
+  type = number
+  default = 8
+}
+
+variable "ipv4_address" {
+  type = string
+}
+
+variable "ipv4_gateway" {
+  type = string
+}
+
+variable "user_account_keys" {
+  type = list(string)
+}
+
+variable "user_account_username" {
+  type = string
+}
+
+variable "memory_dedicated" {
+  type = number
+}
+
+variable "operating_system_type" {
+  type = string
+  default = "l26"
+}
+
+variable "startup_order" {
+  type    = number
+  default = 3
+}
+
+variable "startup_up_delay" {
+  type    = number
+  default = 60
+}
+
+variable "startup_down_delay" {
+  type    = number
+  default = 60
+}
