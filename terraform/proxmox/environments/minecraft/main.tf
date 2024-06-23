b8b3afe9efe60bce1e534f4221016325d7305ff1
snake <admin@viasnake.com>
2024-06-24 00:22:16 +0900

    feat: minecraft1001 を追加

--- terraform/proxmox/environments/minecraft/main.tf
+++ terraform/proxmox/environments/minecraft/main.tf
@@ -1,33 +1,27 @@
-module "minecraft1004" {
-  source = "../../modules/qemu"
+module "minecraft1001" {
+  source = "../../modules/lxc"
 
-  name         = "minecraft1004"
-  target_node  = "kitsune"
-  vmid         = 0
-  clone        = "ubuntu-server-22.04"
-  cores        = 4
-  sockets      = 1
-  memory       = 8192
-  os_type      = "cloud-init"
-  disk_type    = "scsi"
-  disk_storage = "local-lvm"
-  disk_size    = "128G"
-  ipconfig0    = "ip=10.210.30.4/24,gw=10.210.0.1"
+  hostname       = "minecraft1001"
+  target_node    = "kitsune"
+  memory         = 1024
+  swap           = 1024
+  network_ip     = "10.210.30.1/24"
+  rootfs_size    = "8G"
+  rootfs_storage = "Synology-01-LUN-01"
+  nesting        = true
 }
 
-module "minecraft1005" {
-  source = "../../modules/qemu"
+# module "minecraft1002" {
+#   source = "../../modules/vm"
 
-  name         = "minecraft1005"
-  target_node  = "kitsune"
-  vmid         = 0
-  clone        = "ubuntu-server-22.04"
-  cores        = 4
-  sockets      = 1
-  memory       = 4096
-  os_type      = "cloud-init"
-  disk_type    = "scsi"
-  disk_storage = "local-lvm"
-  disk_size    = "128G"
-  ipconfig0    = "ip=10.210.30.5/24,gw=10.210.0.1"
-}
+#   name        = "minecraft1001"
+#   target_node = "kitsune"
+#   full_clone  = false
+#   memory      = 8192
+#   cores       = 4
+#   ciuser      = var.ciuser
+#   cipassword  = var.cipassword
+#   ipconfig0   = "ip=10.210.30.1/24,gw=10.210.0.1"
+#   storage     = "local-lvm"
+#   size        = "128G"
+# }
