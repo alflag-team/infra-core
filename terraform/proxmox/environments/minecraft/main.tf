f74092038319d4f897bca3d4e8c41a0b42842a80
snake <admin@viasnake.com>
2024-07-21 22:48:23 +0900

    fix: メモリが不足するので割当増加

--- terraform/proxmox/environments/minecraft/main.tf
+++ terraform/proxmox/environments/minecraft/main.tf
@@ -16,8 +16,8 @@ module "minecraftproxy1001" {
 
   hostname       = "minecraftproxy1001"
   target_node    = "kitsune"
-  memory         = 1024
-  swap           = 1024
+  memory         = 2048
+  swap           = 8192
   network_ip     = "10.210.30.101/24"
   rootfs_size    = "8G"
   rootfs_storage = "Synology-01-LUN-01"
