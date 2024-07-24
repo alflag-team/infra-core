d933f755c04e01dc1ba0d83fba5faabd79b6add6
snake <admin@viasnake.com>
2024-07-25 06:41:43 +0900

    fix: ディスクサイズを 8GB に変更

--- terraform/proxmox/environments/unbound/main.tf
+++ terraform/proxmox/environments/unbound/main.tf
@@ -6,7 +6,7 @@ module "unbound1001" {
   memory         = 1024
   swap           = 8192
   network_ip     = "10.210.9.1/24"
-  rootfs_size    = "16G"
+  rootfs_size    = "8G"
   rootfs_storage = "Synology-01-LUN-01"
   nesting        = true
 }
