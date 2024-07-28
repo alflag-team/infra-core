eedca1a6775dffca8468aa75489c806319ec5f1e
snake <admin@viasnake.com>
2024-07-28 09:41:58 +0900

    Submodule terraform-proxmox

--- terraform/proxmox/environments/gateway/main.tf
+++
@@ -1,9 +0,0 @@
-module "gateway1001" {
-  source = "../../modules/lxc"
-
-  hostname      = "gateway1001"
-  target_node   = "kitsune"
-  memory        = 1024
-  network_ip    = "10.210.3.1/24"
-  nesting       = true
-}
